5cc466aeddedbd8e2dd3a88c70e2ffddfeb1766a
victor holl <v_h@me.com>
2020-05-12 16:23:48 +0200

    nice

---
+++ db/migrate/20200512132618_add_columns_to_articles.rb
@@ -0,0 +1,6 @@
+class AddColumnsToArticles < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :title, :string
+    add_column :articles, :content, :text
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Firing up"
+10.times do
+  article = Article.new(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraphs.join)
+  article.save
+end
+puts "All done"
